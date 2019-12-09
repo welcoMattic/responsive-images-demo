6444918e3102464b75d43095edcd93ff026e7c66
Mathieu Santostefano <msantostefano@jolicode.com>
2019-12-09 20:02:52 +0100

    Fix trusted proxies

--- public/index.php
+++ public/index.php
@@ -12,12 +12,8 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-if ($trustedProxies = $request->server->get('CC_REVERSE_PROXY_IPS')) {
-    // trust *all* requests
-    Request::setTrustedProxies(array_merge(['127.0.0.1'], explode(',', $trustedProxies)),
-
-    // trust *all* "X-Forwarded-*" headers
-    Request::HEADER_X_FORWARDED_ALL);
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
 }
 
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
